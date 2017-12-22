729cf73f9c486a5996336f18f4af61024656c25a
Irsan Sebastian <irsansebastianc4@gmail.com>
2017-12-22 12:09:13 +0700

    adding testing for article.js

--- server/test/article.js
+++ server/test/article.js
@@ -7,28 +7,28 @@ chai.use(chaiHttp)
 const app = 'http://localhost:3000'
 
 describe('Testing Article', function () {
-  // it('result - Post an article', function (done) {
-  //   chai.request(app)
-  //     .post('/article')
-  //     .send({
-  //       title: 'This is my Story',
-  //       body: 'This is about my life @hacktiv8',
-  //       author: 'Irsan'
-  //     })
-  //     .end(function (err, res) {
-  //       expect(res.body).to.have.property(
-  //         'title'
-  //       )
-  //       done()
-  //     })
-  // })
+  it('result - Post an article', function (done) {
+    chai.request(app)
+      .post('/article')
+      .send({
+        title: 'This is my Story',
+        body: 'This is about my life @hacktiv8',
+        author: 'Irsan'
+      })
+      .end(function (err, res) {
+        expect(res.body).to.have.property(
+          'title'
+        )
+        done()
+      })
+  })
   // DIUNCOMMENT APABILA UNTUK MENGECEK BISA POST ARTICLE ATAU TIDAK
 
   it('result - Get all article', function (done) {
     chai.request(app)
       .get('/articles')
       .end(function (err, res) {
-        expect(res.body).to.have.lengthOf(6)
+        expect(res.body).to.have.lengthOf(1)
         done()
       })
   })
